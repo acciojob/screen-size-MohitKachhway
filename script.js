7f9f347c341ead334363979e92c2040a7520877b
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-12 10:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let x= document.getElementById("x");
+let y= document.getElementById("y");
+x.innerText = window.innerWidth;
+y.innerText = window.innerWidth;
+
+
+// h1.textHTML =Width: x and Height: y
+
+window.addEventListener("resize" , ()=>{
+  x.innerText = window.innerWidth;
+  y.innerText = window.innerHeight;
+})
